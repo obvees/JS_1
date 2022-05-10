488c78bbaa9b10e8447be2c79bcaf066386eb1fd
Obvees <101640077+obvees@users.noreply.github.com>
2022-05-10 11:48:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+(function () {
+  const example = document.getElementById('example')
+  const cw1 = document.getElementById('cw1')
+  const cw2 = document.getElementById('cw2')
+  const cw3 = document.getElementById('cw3')
+  const answer = document.getElementById('answer')
+
+  example.addEventListener("click", function () {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        console.log(array)
+        answer.innerHTML = JSON.stringify(array);
+      })
+  })
+
+    cw1.addEventListener("click", function () {
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(function (data){
+          appendData(data);
+        })
+    function appendData(data){
+      answer.innerHTML = "";
+      for (var i = 0; i <data.length; i++){
+        var div = document.createElement("div");
+        div.innerHTML= '<strong>userid:' 
+          + data[i].userid +  '</strong> <strong>id:' 
+          + data[i].id +  '</strong> <br> <strong>title:</strong>' 
+          + data[i].title +  '<br> <strong>body:</strong>'
+          + data[i].body + '<br><br>';
+        answer.appendChild(div);
+      }
+    }
+  })
+
+  cw2.addEventListener("click", function () {
+    answer.innerHTML = "Loading...";
+  })
+
+  cw3.addEventListener("click", function () {
+   answer.innerHTML = "Loading...";
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then 
+  })
+
+})();
